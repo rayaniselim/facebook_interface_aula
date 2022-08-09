c21925aafd59e25853d919930365ecee83607ac0
rayaniselim <rayaniselim@outlook.com>
2022-08-09 11:45:32 -0400

    aula 36

---
+++ lib/componentes/botao_circulo.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BotaoCirculo extends StatelessWidget {
+  const BotaoCirculo(
+      {Key? key,
+      required this.icone,
+      required this.iconeTamanho,
+      required this.onPressed})
+      : super(key: key);
+
+  final IconData icone;
+  final double iconeTamanho;
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        shape: BoxShape.circle,
+      ),
+      margin: EdgeInsets.all(6),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(icone),
+        iconSize: iconeTamanho,
+        color: Colors.black,
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,56 @@
+import 'package:facebook_interface_aula/componentes/botao_circulo.dart';
+import 'package:facebook_interface_aula/uteis/paleta_cores.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            //expandedHeight: 250,
+            floating: true, // rolagem da tela
+            centerTitle: false,
+            title: Text(
+              'Facebook',
+              style: TextStyle(
+                color: PaletaCores.azulFacebook,
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                letterSpacing: -1.2,
+              ),
+            ),
+            actions: [
+              BotaoCirculo(
+                icone: Icons.search,
+                iconeTamanho: 30,
+                onPressed: () {},
+              ),
+              BotaoCirculo(
+                icone: LineIcons.facebookMessenger,
+                iconeTamanho: 30,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              color: Colors.pink,
+              height: 1000,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
